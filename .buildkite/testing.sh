c33055ab457d72071a0e0a0e9933ee5fc53b8d62
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-07-04 15:40:45 +0530

    test

--- .buildkite/testing.sh
+++ .buildkite/testing.sh
@@ -3,8 +3,10 @@
 get_service_details() {
     local service_name=$1
     local detail=$2
-    jq -r --arg service "$service_name" --arg detail "$detail" '.[$service][$detail]' service.json
+    jq -r --arg service "$service_name" --arg detail "$detail" '.[$service][$detail]' $json_file
 }
+
+json_file="C:\\buildkite-agent\\builds\\RTC-DLLA48-1\\rtctek\\deployment-pipeline\\.buildkite\\services.json"
 
 echo "jq version"
 jq --version
